994528a29e35ff9b78916fa241f7eaef93206ab7
DivineDemon <supame123@gmail.com>
2022-07-13 23:26:50 +0400

    Routes and Controllers! ✨

--- server/index.js
+++ server/index.js
@@ -3,9 +3,22 @@ import "dotenv/config";
 
 import { connectDB } from "./config/db.js";
 
+// Import Routes
+import userRoute from "./routes/users.js";
+import videoRoute from "./routes/videos.js";
+import commentRoute from "./routes/comments.js";
+import authRoute from "./routes/auth.js";
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Routes
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/videos", videoRoute);
+app.use("/api/comments", commentRoute);
+
+// Server Listening + DB Connection
 app.listen(PORT, () => {
   console.log(`Server Listening on Port: ${PORT}`);
   connectDB();
