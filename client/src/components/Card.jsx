5fd63169e17895879456f5f1f4a1840885325853
DivineDemon <supame123@gmail.com>
2022-07-17 23:56:18 +0400

    Roboto Font! ✨

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -26,12 +26,36 @@ const ChannelImage = styled.img`
   background-color: #ccc;
 `;
 
+const Texts = styled.div``;
+
+const Title = styled.h1`
+  font-size: 16px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.text};
+`;
+
+const ChannelName = styled.h2`
+  font-size: 14px;
+  color: ${({ theme }) => theme.textSoft};
+  margin: 9px 0px;
+`;
+
+const Info = styled.div`
+  font-size: 14px;
+  color: ${({ theme }) => theme.textSoft};
+`;
+
 const Card = () => {
   return (
     <Container>
       <Image />
       <Details>
         <ChannelImage />
+        <Texts>
+          <Title>Test Video</Title>
+          <ChannelName>ShoodaCoder</ChannelName>
+          <Info>660,998 views • 1 day ago</Info>
+        </Texts>
       </Details>
     </Container>
   );
